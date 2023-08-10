750a626bf54db86d725ace17088d3d67814bb07a
theberdakh <theberdakh@gmail.com>
2023-08-10 23:57:50 +0500

    create Phone Register Fragment

---
+++ app/src/main/java/com/theberdakh/carrierapp/ui/PhoneRegisterFragment.kt
@@ -0,0 +1,15 @@
+package com.theberdakh.carrierapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.theberdakh.carrierapp.R
+import com.theberdakh.carrierapp.databinding.FragmentPhoneRegisterBinding
+
+class PhoneRegisterFragment: Fragment(R.layout.fragment_phone_register) {
+    private lateinit var binding: FragmentPhoneRegisterBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentPhoneRegisterBinding.bind(view)
+    }
+}
