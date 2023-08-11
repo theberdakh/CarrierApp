d2ce4f5ebc58ac7fef70dd1d2c7162e6ac673143
theberdakh <theberdakh@gmail.com>
2023-08-11 16:59:13 +0500

    create Phone Confirmation Fragment with simple edit text

---
+++ app/src/main/java/com/theberdakh/carrierapp/ui/PhoneConfirmationFragment.kt
@@ -0,0 +1,15 @@
+package com.theberdakh.carrierapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.theberdakh.carrierapp.R
+import com.theberdakh.carrierapp.databinding.FragmentPhoneConfirmationBinding
+
+class PhoneConfirmationFragment(): Fragment(R.layout.fragment_phone_confirmation) {
+private lateinit var binding: FragmentPhoneConfirmationBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentPhoneConfirmationBinding.bind(view)
+    }
+}
