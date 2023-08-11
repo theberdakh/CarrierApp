d4903a80b19238d3e273351e04bab885244eab6b
theberdakh <theberdakh@gmail.com>
2023-08-11 17:23:58 +0500

    create my nav with four screens

---
+++ app/src/main/java/com/theberdakh/carrierapp/ui/WaitFragment.kt
@@ -0,0 +1,16 @@
+package com.theberdakh.carrierapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.theberdakh.carrierapp.R
+import com.theberdakh.carrierapp.databinding.FragmentWaitBinding
+
+class WaitFragment: Fragment(R.layout.fragment_wait) {
+    private lateinit var binding: FragmentWaitBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWaitBinding.bind(view)
+
+    }
+}
