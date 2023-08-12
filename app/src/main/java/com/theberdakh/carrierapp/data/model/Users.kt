fc6d585e98b841cc4a14fce2afd191b04bd507fc
theberdakh <theberdakh@gmail.com>
2023-08-12 18:19:29 +0500

    create fragment login

---
+++ app/src/main/java/com/theberdakh/carrierapp/data/model/Users.kt
@@ -0,0 +1,13 @@
+package com.theberdakh.carrierapp.data.model
+
+data class Seller(
+    val name: String
+)
+
+data class TaxManager(
+    val name: String
+)
+
+data class Carrier(
+    val name: String
+)
