eb3cdca39e8e028e366d6bec594a61b907ee0c39
theberdakh <theberdakh@gmail.com>
2023-08-13 16:56:07 +0500

    fix seller and tax officer forms

--- app/src/main/java/com/theberdakh/carrierapp/ui/authentication/SignUpFragment.kt
+++ app/src/main/java/com/theberdakh/carrierapp/ui/authentication/SignUpFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.tabs.TabLayoutMediator
 import com.theberdakh.carrierapp.R
 import com.theberdakh.carrierapp.databinding.FragmentSignUpBinding
+import com.theberdakh.carrierapp.ui.authentication.adapter.SignUpViewPagerAdapter
 
 class SignUpFragment: Fragment(R.layout.fragment_sign_up) {
     private lateinit var binding: FragmentSignUpBinding
@@ -15,7 +17,20 @@ class SignUpFragment: Fragment(R.layout.fragment_sign_up) {
         binding = FragmentSignUpBinding.bind(view)
 
         setListeners()
+        setViews()
 
+    }
+
+    private fun setViews() {
+      binding.vpSignUp.adapter = SignUpViewPagerAdapter(arrayListOf(SellerSignUpFragment(), TaxOfficerSignUpFragment()), requireActivity().supportFragmentManager, requireActivity().lifecycle)
+
+      TabLayoutMediator(binding.tblSignUp, binding.vpSignUp){ tab, position ->
+          when(position){
+              0 -> tab.text = "Satıwshı"
+              1 -> tab.text = "Salıq xizmetshisi"
+          }
+
+      }.attach()
     }
 
     private fun setListeners() {
--- app/src/main/java/com/theberdakh/carrierapp/ui/authentication/adapter/SignUpViewPagerAdapter.kt
+++ app/src/main/java/com/theberdakh/carrierapp/ui/authentication/adapter/SignUpViewPagerAdapter.kt
@@ -1,4 +1,12 @@
 package com.theberdakh.carrierapp.ui.authentication.adapter
 
-class SignUpViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SignUpViewPagerAdapter(private val list: ArrayList<Fragment>, fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount() = list.size
+    override fun createFragment(position: Int) = list[position]
+
 }
